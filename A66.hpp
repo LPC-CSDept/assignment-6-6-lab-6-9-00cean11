5f5b127b3aa630e9c5ca9f780ea2533d2d0d53b5
00cean11 <oceanisnotsea@gmail.com>
2022-10-13 17:06:38 -0700

    A66

--- A66.hpp
+++ A66.hpp
@@ -3,5 +3,45 @@
 #include <iomanip>
 #include <iostream>
 using namespace std;
+void getinput(int &, int &, int &);
+void maxmin(int, int, int, int &, int &);
+int main (){
+  int n1, n2, n3;
+  int max, min;
+  getinput( n1,  n2,  n3);
+  maxmin( n1,  n2,  n3,  max,  min);
+  
+  cout << max << ", " << min << ", " << max - min <<endl;
+}
+void getinput(int &n1, int &n2, int &n3){
+    cin >> n1 >> n2 >> n3;
+}
+void maxmin(int n1, int n2, int  n3, int &max, int &min){
+	if (n1 < n2 && n1 < n3){
+		if (n2 < n3){
+			min = n1;
+			max = n3;
+		} else {
+			min = n1;
+			max = n2;
+		}
+	} else if (n2 < n1 && n2 < n3){
+		if (n1 < n3){
+			min = n2;
+			max = n3;
+		} else {
+			min = n2;
+			max = n1;
+		}
+	} else if (n3 < n1 && n3 < n2){
+		if (n1 < n2){
+			min = n3;
+			max = n2;
+		} else {
+			min = n3;
+			max = n1;
+		}
+	}
+}
 
 // complete the function maxmin( )
